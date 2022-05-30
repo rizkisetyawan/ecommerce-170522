879c27545abfd09090c53e39cff05561c783cd40
taufikAlex <taufik_alex@yahoo.com>
2022-05-30 09:17:26 +0700

    [update] ROUTES

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {
-  Home,
-  Login,
-  Register,
-  Product,
-} from './pages';
+import { Home, Login, Register, Product } from './pages';
 import { Topbar } from './components';
 
 function App() {
@@ -15,6 +10,9 @@ function App() {
         <Route path="/" element={<Topbar />}>
           <Route index element={<Home />} />
           <Route path="/product" element={<Product />} />
+        </Route>
+        <Route>
+          <Route path="/dashboard" element={<Product />} />
         </Route>
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
